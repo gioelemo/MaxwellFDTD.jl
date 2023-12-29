12b4b53854b2cb10dbbca48239d701bc0020c5d3
Class04 Course <class209@daint105.login.cscs.ch>
2023-12-29 18:29:57 +0100

    add some docs for 2D

--- scripts/2D_maxwell_pml_xPU.jl
+++ scripts/2D_maxwell_pml_xPU.jl
@@ -11,18 +11,33 @@ using Printf, Plots, JLD
 plot_font = "Computer Modern"
 default(fontfamily=plot_font, framestyle=:box, label=true, grid=true, labelfontsize=11, tickfontsize=11, titlefontsize=13)
 
+"""
+    update_Ex!(Ex, Hz, σ, ε0, dt, dy)
+
+Update the Ex field
+"""
 @parallel function update_Ex!(Ex, Hz, σ, ε0, dt, dy)
     #Ex[:, 2:end-1] .+= dt / ε0 .* (-σ .* Ex[:, 2:end-1] .+ diff(Hz, dims=2)./ dy)
-    @inn_y(Ex) = @inn_y(Ex) .+ dt / ε0 .* (-σ .* @inn_y(Ex) .+ @d_ya(Hz) ./ dy)
+    @inn_y(Ex) = @inn_y(Ex) + dt / ε0 * (-σ * @inn_y(Ex) + @d_ya(Hz) / dy)
     return nothing
 end
 
+"""
+    update_Ey!(Ey, Hz, σ, ε0, dt, dx)
+
+Update the Ey field
+"""
 @parallel function update_Ey!(Ey, Hz, σ, ε0, dt, dx)
     #Ey[2:end-1, :] .+= dt / ε0 .* (-σ .* Ey[2:end-1, :] .- diff(Hz, dims=1) ./ dx)
-    @inn_x(Ey) = @inn_x(Ey) .+ dt / ε0 .* (-σ .* @inn_x(Ey) .- @d_xa(Hz) ./ dx)
+    @inn_x(Ey) = @inn_x(Ey) + dt / ε0 * (-σ * @inn_x(Ey) - @d_xa(Hz) / dx)
     return nothing
 end
 
+"""
+    update_PML_x!(pml_width, pml_alpha, Ex)
+
+Update the x-regions of the pml
+"""
 @parallel_indices (i,j) function update_PML_x!(pml_width, pml_alpha, Ex)
     # for i in 1:pml_width
     #     Ex[i, :] .= exp(-(pml_width - i) * pml_alpha) .* Ex[i, :]
@@ -36,6 +51,11 @@ end
     return nothing
 end
 
+"""
+    update_PML_y!(pml_width, pml_alpha, Ey)
+
+Update the y-regions of the pml
+"""
 @parallel_indices (i,j) function update_PML_y!(pml_width, pml_alpha, Ey)
     # for i in 1:pml_width
     #     Ex[i, :] .= exp(-(pml_width - i) * pml_alpha) .* Ex[i, :]
@@ -50,25 +70,44 @@ end
     return nothing
 end
 
+"""
+    update_Hz!(Hz, Ex, Ey, σ, μ0, dt, dy, dx)
+
+Update the Hz field
+"""
 @parallel function update_Hz!(Hz, Ex, Ey, σ, μ0, dt, dy, dx)
     #Hz .+= dt / μ0 .* (-σ .* Hz .+ diff(Ex, dims=2) ./ dy .- diff(Ey, dims=1) ./ dx)
-    @all(Hz) = @all(Hz) .+ dt / μ0 .* (-σ .* @all(Hz) .+ @d_ya(Ex) ./ dy .- @d_xa(Ey) ./ dx)
+    @all(Hz) = @all(Hz) + dt / μ0 * (-σ * @all(Hz) + @d_ya(Ex) / dy - @d_xa(Ey) / dx)
     return nothing
 end
     
+"""
+    maxwell(ny_, nt_, nvis_, pml_alpha_; do_visu=false, do_check=true, do_test=true)
+
+Use the Finite Difference Time Domain (FDTD) solver to solve Maxwell's equations
+
+# Arguments
+- `ny_::Integer`: Number of y discretization-steps.
+- `nt_::Integer`: Number of timesteps.
+- `nvis_::Integer`: Number of steps between visualisation output.
+- `pml_alpha_::Float` : "Strength" of the PML layer
+- `do_visu::Boolean=false`: Perform visualisation.
+- `do_check::Boolean=true` : Perform advance checks
+- `do_test::Boolean=false`: Perform testing (generate a jld reference file).
+"""
 @views function maxwell(ny_, nt_, nvis_, pml_alpha_; do_visu=false, do_check=true, do_test=true)
     # physics
-    lx, ly = 40.0, 40.0
-    ε0 = 1.0
-    μ0 = 1.0
-    σ = 1.0
+    lx, ly = 40.0, 40.0       # physical size
+    ε0 = 1.0                  # permittivity
+    μ0 = 1.0                  # permeability
+    σ = 1.0                   # electrical conductivity
     
     # numerics
-    nx, ny = ny_ - 1, ny_
+    nx, ny = ny_ - 1, ny_     # number space steps
 
     # PML parameters
-    pml_width = 50
-    pml_alpha = pml_alpha_
+    pml_width = 50            # PML extensions
+    pml_alpha = pml_alpha_    # PML "strength"
      
     # Extend the grid
     nx_pml, ny_pml = nx + 2 * pml_width, ny + 2 * pml_width
@@ -145,3 +184,5 @@ end
 #maxwell(256, 15000, 100, 0.0; do_visu=true, do_test=false)
 #maxwell(256, 15000, 100, 5.0; do_visu=true, do_test=false)
 #maxwell(256, 15000, 100, 0.1; do_visu=true, do_test=false)
+
+#maxwell(50, 10, 10, 0.25; do_visu=true, do_test=false)
