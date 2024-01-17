90b36b6239ab819fabdeed5b0c340a32ae6d5af0
Class04 Course <class209@daint103.login.cscs.ch>
2024-01-17 14:10:35 +0100

    cleanup and file ordering

--- scripts/run_3D_maxwell_pml_multixPU.sh
+++ scripts/run_3D_maxwell_pml_multixPU.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
-#SBATCH --job-name="3D_maxwell_pml_xPU"
-#SBATCH --output=3D_maxwell_pml_xPU.%j.o
-#SBATCH --error=3D_maxwell_pml_xPU.%j.e
+#SBATCH --job-name="3D_maxwell_pml_multixPU"
+#SBATCH --output=3D_maxwell_pml_mutlixPU.%j.o
+#SBATCH --error=3D_maxwell_pml_multixPU.%j.e
 #SBATCH --time=00:30:00
-#SBATCH --nodes=1
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=normal
 #SBATCH --constraint=gpu
@@ -12,4 +12,4 @@
 module load daint-gpu
 module load Julia/1.9.3-CrayGNU-21.09-cuda
 
-srun julia -O3 3D_maxwell_combined.jl
+srun julia -O3 3D_maxwell_combined_mpi.jl
